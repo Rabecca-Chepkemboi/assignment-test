093313ce50dd4a8d34f509487beb24c826a3a3bf
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-03-09 09:00:26 +0300

    do a trial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main() {
+    var result = names("Jane", "Mary")
+    println(result)
+
+    detail()
+
+    var answer = people("Mercy", "James", "Ken")
+    println(answer)
+
+}
+
+fun names(w: String, x: String): String{
+    //var name = w.length
+    return names(w,x)
+}
+
+fun numbers(num1: Int, num2: Int, num3: Int):Array<Int>{
+    var number = (45, 34, 56)
+}
+
+fun detail(){
+    var details = ("Anything can change")
+    println(details)
+}
+
+fun people(a : String, b: String, c: String):Array<String>{
+    return people(a, b, c)
+}
+
+class Calculator(){
+    num
+}
+
